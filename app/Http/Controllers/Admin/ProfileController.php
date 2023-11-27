fb35e7bb91f965884909025a78859573f003a6db
kanakom-ishibashi <kanakom001@gmail.com>
2023-11-27 02:30:08 +0000

    ProfileController.php

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,19 +7,18 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
-}
-public function add()
+    public function add()
     {
         return view('admin.profile.create');
     }
 
     public function create()
     {
-        return redirect('admin/profile/create');
+        // プロフィールを作成するためのロジック
     }
 
     public function edit()
     {
         return view('admin.profile.edit');
     }
+}
